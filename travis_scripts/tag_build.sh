222fa8e53b6f324ff132d71932bae97234be0c14
drugotosto <ferrero.maurizio84@gmail.com>
2018-05-17 11:24:52 +0200

    - Aggiunto comandi per eseguire il tagging in locale e successivamente eseguire la push in remoto

--- travis_scripts/tag_build.sh
+++ travis_scripts/tag_build.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 echo "Vado sia a TAGGARE che a creare una nuova GITHUB RELEASE del source code relativo al repository. Come TAG utilizzo: $BUILD_NAME"
 
+echo"Prima taggo in locale il commit appena eseguito"
+git tag -a $BUILD_NAME -m "creazione nuovo tag"
+echo "Successivemante faccio la push di tale tag anche in remoto"
+git push github $BUILD_NAME
+
+echo "A questo punto vado a creare una nuova GitHub release"
 export TARGET_URL="https://api.github.com/repos/drugotosto/spmia-chapter4/releases?access_token=$GITHUB_TOKEN"
 
 body="{
   \"tag_name\": \"$BUILD_NAME\",
   \"target_commitish\": \"master\",
   \"name\": \"$BUILD_NAME\",
-    \"body\": \"Release of version $BUILD_NAME\",
+  \"body\": \"Release of version $BUILD_NAME\",
   \"draft\": true,
   \"prerelease\": true
 }"
