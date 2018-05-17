d8d86d71b81e439d5f2b2e2c4ee283dfa4169491
drugotosto <ferrero.maurizio84@gmail.com>
2018-05-17 16:22:40 +0200

    - Inserimento commenti.... (La creazione della release tramite GitHub API non funziona)

--- travis_scripts/tag_build.sh
+++ travis_scripts/tag_build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 echo "Vado sia a TAGGARE che a creare una nuova GITHUB RELEASE del source code relativo al repository. Come TAG utilizzo: $BUILD_NAME"
 
-#echo "Successivemante faccio la push di tale tag anche in remoto"
-#git push https://github.com/drugotosto/spmia-chapter4 $BUILD_NAME
-
 export TARGET_URL="https://api.github.com/repos/drugotosto/spmia-chapter4/releases?access_token=$GITHUB_TOKEN"
 
 body="{
